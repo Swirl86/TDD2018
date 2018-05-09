cc8985cd5d879880914e5008432c4c3c4eeed608
Max Karlson <maaxkarlson@live.se>
2018-05-09 09:47:26 +0200

    Added the testfile containing a test which fails at creating the game class.

---
+++ TestBowlingGame.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/*
+ *  Test case for user story createNewBowlingGame
+ *  TDD JUnit Bowling game 2018 PA1417
+ *  In this assignment your group should develop software for calculating score of a single bowling game. 
+ *  You must use Test Driven Development approach and use of JUnit framework for automating testing scripts.
+ */
+
+public class TestBowlingGame {
+
+    @Test
+    public void createNewBowlingGame() {
+        BowlingGame game = new BowlingGame();
+    }
+}
