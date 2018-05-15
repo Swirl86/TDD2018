42d2b7651686891593060c65038eeeaca79b9dbd
Fredrik Olsson <fredde665@gmail.com>
2018-05-15 16:42:10 +0200

    Refactoring BowlingGame class to more logical tasks

--- BowlingGame.java
+++ BowlingGame.java
@@ -3,16 +3,21 @@
  */
 
 public class BowlingGame {
-
+	private int[] rolls = new int[21];
+	private int roll = 0;
 	private int score = 0;
 	
 	public void roll(int nrOfPinsKnockedDown) {
-		score += nrOfPinsKnockedDown;
+		rolls[roll++] = nrOfPinsKnockedDown;
 	}
 	
 	public int score() {
-		return score;
-	}
+		for (int i = 0; i < rolls.length; i++) {
+			score += rolls[i];
+		}
+	return score;
+}
+
 }
 
 
