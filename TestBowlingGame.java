d2f93a4ea0d3f8914250bd7281edf74980675a58
Fredrik Olsson <fredde665@gmail.com>
2018-05-15 15:49:32 +0200

    Refactoring of user story testGetAScore: Removed unnecessary variables

--- TestBowlingGame.java
+++ TestBowlingGame.java
@@ -1,4 +1,8 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+
 
 /*
  * Main file for testing Laboration 5
@@ -9,27 +13,21 @@ import org.junit.Test;
  *  TDD JUnit Bowling game 2018 PA1417
  *  In this assignment your group should develop software for calculating score of a single bowling game. 
  *  You must use Test Driven Development approach and use of JUnit framework for automating testing scripts.
-*/
+ */
 
 public class TestBowlingGame {
 
-    @Test
-    public void createNewBowlingGame() {
-        BowlingGame game = new BowlingGame();
+    BowlingGame game;
+    
+    @Before
+        public void setUp() {
+        game = new BowlingGame();
     }
-
+    
     @Test
-    public void rollABowlingBall() {
-	    BowlingGame game = new BowlingGame();
+        public void testGetGameScore() {
         game.roll(0);
-	}
-
-    @Test
-    public void rollABowlingBallGetAScore() {
-    	BowlingGame game = new BowlingGame();
-    	game.roll(0);
-    	assertEquals(0, game.score());
-    }
-
-
+        assertEquals(0, game.score());
+        }
 }
+    
