6445a45f7ac29522690b711bfd1e94989b998590
Fredrik Olsson <fredde665@gmail.com>
2018-05-16 09:11:37 +0200

    Refactoring BowlingGame class for special checks

--- BowlingGame.java
+++ BowlingGame.java
@@ -3,6 +3,7 @@
  */
 
 public class BowlingGame {
+	private static final int MAXFRAMES = 10;
 	private int rolls[] = new int[21];
 	private int aRoll = 0;
 	private int score = 0;
@@ -13,18 +14,38 @@ public class BowlingGame {
 
 	public int score() {
 		int frameNr = 0;
-		for (int frame = 0; frame < 10; frame++) {
-			if (rolls[frameNr] == 10) {
-				score += 10 + rolls[frameNr + 1] + rolls[frameNr + 2];
+		for (int frame = 0; frame < MAXFRAMES; frame++) {
+			if (isAStrike(frameNr)) {
+				score += strikeScore(frameNr);
 				frameNr++;
-			} else if (rolls[frameNr] + rolls[frameNr + 1] == 10) {
-				score += 10 + rolls[frameNr + 2];
+			} else if (isASpare(frameNr)) {
+				score += spareScore(frameNr);
 				frameNr += 2;
 			} else {
-				score += rolls[frameNr] + rolls[frameNr + 1];
+				score += sumOfFrameScore(frameNr);
 				frameNr += 2;
 			}
 		}
 		return score;
+	}
+
+	private boolean isASpare(int frameNr) {
+		return sumOfFrameScore(frameNr) == 10;
+	}
+
+	private boolean isAStrike(int frameNr) {
+		return rolls[frameNr] == 10;
+	}
+
+	private int spareScore(int frameNr) {
+		return 10 + rolls[frameNr + 2];
+	}
+
+	private int strikeScore(int frameNr) {
+		return 10 + rolls[frameNr + 1] + rolls[frameNr + 2];
+	}
+
+	private int sumOfFrameScore(int frameNr) {
+		return rolls[frameNr] + rolls[frameNr + 1];
 	}
 }
