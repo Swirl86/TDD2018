da37a343cd3d629c68ad996cc48f495db669308c
Fredrik Olsson <fredde665@gmail.com>
2018-05-16 10:28:21 +0200

    Test case for user story BonusIsAStrike

--- TestBowlingGame.java
+++ TestBowlingGame.java
@@ -207,4 +207,41 @@ public class TestBowlingGame {
 
 	}
 
+	@Test
+	public void BonusIsAStrike() {
+
+		game.roll(1);
+		game.roll(5);
+
+		game.roll(3);
+		game.roll(6);
+
+		game.roll(7);
+		game.roll(2);
+
+		game.roll(3);
+		game.roll(6);
+
+		game.roll(4);
+		game.roll(4);
+
+		game.roll(5);
+		game.roll(3);
+
+		game.roll(3);
+		game.roll(3);
+
+		game.roll(4);
+		game.roll(5);
+
+		game.roll(8);
+		game.roll(1);
+
+		game.roll(2);
+		game.roll(8);
+		game.roll(10);
+		assertEquals(93, game.score());
+
+	}
+
 }
