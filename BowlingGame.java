864dc362c6217064757fea62327f6fcf72dfd44d
Fredrik Olsson <fredde665@gmail.com>
2018-05-16 09:07:42 +0200

    Implementation of user story testDoAStrike

--- BowlingGame.java
+++ BowlingGame.java
@@ -6,15 +6,18 @@ public class BowlingGame {
 	private int rolls[] = new int[21];
 	private int aRoll = 0;
 	private int score = 0;
-	
+
 	public void roll(int nrOfPinsKnockedDown) {
 		rolls[aRoll++] = nrOfPinsKnockedDown;
 	}
-	
+
 	public int score() {
 		int frameNr = 0;
 		for (int frame = 0; frame < 10; frame++) {
-			if (rolls[frameNr] + rolls[frameNr + 1] == 10) {
+			if (rolls[frameNr] == 10) {
+				score += 10 + rolls[frameNr + 1] + rolls[frameNr + 2];
+				frameNr++;
+			} else if (rolls[frameNr] + rolls[frameNr + 1] == 10) {
 				score += 10 + rolls[frameNr + 2];
 				frameNr += 2;
 			} else {
@@ -25,6 +28,3 @@ public class BowlingGame {
 		return score;
 	}
 }
-
-
-
